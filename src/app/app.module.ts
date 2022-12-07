6ee9e60fbc679354309304090107e306652a7ddd
MarcusBYUI <ojo18002@byui.edu>
2022-12-07 13:32:47 +0100

    added header and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PostComponent } from './post/post.component';
+import { HeaderComponent } from './header/header.component';
+import { PostEditComponent } from './post/post-edit/post-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostComponent,
+    HeaderComponent,
+    PostEditComponent
   ],
   imports: [
     BrowserModule,
